8e985a54c2078b9db54c45eb8c659b88b2e25a18
Aurora Sanderson <AuroraTuring@gmail.com>
2024-01-02 20:27:30 -0700

    Add Ship #initialize and #sunk.

---
+++ lib/ship.rb
@@ -0,0 +1,15 @@
+class Ship 
+  attr_reader :name, :length, :health, :sunk
+
+  def initialize(name, length)
+    @name = name 
+    @length = length 
+    @health = 3
+    @sunk = false 
+  end
+
+  def sunk?
+    @sunk 
+  end
+
+end
