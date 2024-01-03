0f1b7820b082ff8525beb84e07197c5c4d8c6a78
Aurora Sanderson <AuroraTuring@gmail.com>
2024-01-02 20:25:41 -0700

    Add and remove x's for TDD

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,39 +1,40 @@
 require './lib/ship'
+require 'rspec'
 
 RSpec.describe Ship do
-  before do 
-    cruiser = Ship.new("Cruiser", 3)
+  before 'each' do 
+    @cruiser = Ship.new("Cruiser", 3)
   end
   it 'exists' do
-    expect(cruiser).to be_an_instance_of(Ship)
+    expect(@cruiser).to be_an_instance_of(Ship)
   end
 
   it 'has readable attributes' do
-    expect(cruiser.name).to eq("Cruiser")
-    expect(cruiser.length).to eq(3)
-    expect(cruiser.health).to eq(3)
+    expect(@cruiser.name).to eq("Cruiser")
+    expect(@cruiser.length).to eq(3)
+    expect(@cruiser.health).to eq(3)
   end
 
   it '#sunk?' do
-    expect(cruiser.sunk?).to be false
+    expect(@cruiser.sunk?).to be false
   end
 
   it '#hit' do
-    cruiser.hit
+    @cruiser.hit
 
-    expect(cruiser.health).to eq(2)
+    expect(@cruiser.health).to eq(2)
       
-    cruiser.hit
+    @cruiser.hit
     
-    expect(cruiser.health).to eq(1)
+    expect(@cruiser.health).to eq(1)
   end
 
-  it 'sinks ship when #hit three times' do
+  xit 'sinks ship when #hit three times' do
 
-    expect(cruiser.sunk?).to be false
+    expect(@cruiser.sunk?).to be false
 
-    cruiser.hit
+    @cruiser.hit
     
-    expect(cruiser.sunk?).to be true
+    expect(@cruiser.sunk?).to be true
   end
 end
