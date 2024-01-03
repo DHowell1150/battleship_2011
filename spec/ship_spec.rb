60c6f25892a3b425cc223ed41ded218a25f05b1e
Dana Howell <howelld115@gmail.com>
2024-01-02 19:27:32 -0700

    Complete tdd for Ship

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,41 +1,39 @@
 require './lib/ship'
 
-text
+RSpec.describe Ship do
+  before do 
+    cruiser = Ship.new("Cruiser", 3)
+  end
+  it 'exists' do
+    expect(cruiser).to be_an_instance_of(Ship)
+  end
 
+  it 'has readable attributes' do
+    expect(cruiser.name).to eq("Cruiser")
+    expect(cruiser.length).to eq(3)
+    expect(cruiser.health).to eq(3)
+  end
 
+  it '#sunk?' do
+    expect(cruiser.sunk?).to be false
+  end
 
+  it '#hit' do
+    cruiser.hit
 
+    expect(cruiser.health).to eq(2)
+    
+    cruiser.hit
+    
+    expect(cruiser.health).to eq(1)
+  end
 
+  it 'sinks ship when #hit three times' do
 
-# cruiser = Ship.new("Cruiser", 3)
-# #=> #<Ship:0x00007feb05112d10...>
+    expect(cruiser.sunk?).to be false
 
-# cruiser.name
-# #=> "Cruiser"
-
-# cruiser.length
-# #=> 3
-
-# cruiser.health
-# #=> 3
-
-# cruiser.sunk?
-# #=> false
-
-# cruiser.hit
-
-# cruiser.health
-# #=> 2
-
-# cruiser.hit
-
-# cruiser.health
-# #=> 1
-
-# cruiser.sunk?
-# #=> false
-
-# cruiser.hit
-
-# cruiser.sunk?
-# #=> true
+    cruiser.hit
+    
+    expect(cruiser.sunk?).to be true
+  end
+end
