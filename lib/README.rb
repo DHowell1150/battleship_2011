fd5273e571ef94adc2aed0144561c0fdbd239b85
Aurora Sanderson <AuroraTuring@gmail.com>
2024-01-02 17:22:28 -0700

    Add Aurora's DTR file link.

--- lib/README.rb
+++ lib/README.rb
@@ -1,3 +1,5 @@
-#Dana's DTR copy:
+# Dana's DTR:
 # https://docs.google.com/document/d/1XhqyAel5j8ORCoVKFv2saiSRO3nEzufkkHoNhnqXaRE/edit?usp=sharing
 
+# Aurora's DTR: 
+# https://docs.google.com/document/d/1VfDXQ8ECUcDevZt-RWIg6agt1maEFy6yYZ1yzfuMDD4/edit?usp=sharing
