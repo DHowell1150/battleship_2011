61366778eb885933deabe033e426e6e3c1b05354
Dana Howell <howelld115@gmail.com>
2024-01-04 15:02:18 -0700

    I2 Instantiate @board object with attributes.

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,22 +1,37 @@
-require './lib/ship'
-require './lib/cell'
+require './lib/board'
+
 
 RSpec.describe Cell do
   before 'each' do 
-
+    @board = Board.new
   end
 
   it 'exists' do
-
+    expect(@board).to be_a(Board)
   end
 
-  it 'has readable attributes' do
-
+  it 'has readable attributes' do #What is tabbing for a hash supposed to look like here?
+  expect(board.cells).to eq({
+      "A1" => #<Cell:0x00007ff0728a3f58...>,
+      "A2" => #<Cell:0x00007ff0728a3ee0...>,
+      "A3" => #<Cell:0x00007ff0728a3e68...>,
+      "A4" => #<Cell:0x00007ff0728a3df0...>,
+      "B1" => #<Cell:0x00007ff0728a3d78...>,
+      "B2" => #<Cell:0x00007ff0728a3d00...>,
+      "B3" => #<Cell:0x00007ff0728a3c88...>,
+      "B4" => #<Cell:0x00007ff0728a3c10...>,
+      "C1" => #<Cell:0x00007ff0728a3b98...>,
+      "C2" => #<Cell:0x00007ff0728a3b20...>,
+      "C3" => #<Cell:0x00007ff0728a3aa8...>,
+      "C4" => #<Cell:0x00007ff0728a3a30...>,
+      "D1" => #<Cell:0x00007ff0728a39b8...>,
+      "D2" => #<Cell:0x00007ff0728a3940...>,
+      "D3" => #<Cell:0x00007ff0728a38c8...>,
+      "D4" => #<Cell:0x00007ff0728a3850...>
+      })
   end
 
-  it 'can #place_ship' do
 
-  end
 end
 
 
@@ -24,102 +39,22 @@ end
 
 
 
-# cell = Cell.new("B4")
-# # => #<Cell:0x00007f84f0ad4720...>
+####
 
-# cell.coordinate
-# # => "B4"
+pry(main)> board.valid_coordinate?("A1")
+# => true
 
-# cell.ship
-# # => nil
+pry(main)> board.valid_coordinate?("D4")
+# => true
 
-# cell.empty?
-# # => true
+pry(main)> board.valid_coordinate?("A5")
+# => false
 
-# cruiser = Ship.new("Cruiser", 3)
-# # => #<Ship:0x00007f84f0891238...>
+pry(main)> board.valid_coordinate?("E1")
+# => false
 
-# cell.place_ship(cruiser)
+pry(main)> board.valid_coordinate?("A22")
+# => false
 
-# cell.ship
-# # => #<Ship:0x00007f84f0891238...>
 
-# cell.empty?
-# # => false
-# Additionally, a cell knows when it has been fired upon. When it is fired upon, the cell’s ship should be damaged if it has one:
 
-# require './lib/ship'
-# # => true
-
-# require './lib/cell'
-# # => true
-
-# cell = Cell.new("B4")
-# # => #<Cell:0x00007f84f0ad4720...>
-
-# cruiser = Ship.new("Cruiser", 3)
-# # => #<Ship:0x00007f84f0891238...>
-
-# cell.place_ship(cruiser)
-
-# cell.fired_upon?
-# # => false
-
-# cell.fire_upon
-
-# cell.ship.health
-# # => 2
-
-# cell.fired_upon?
-# # => true
-# Finally, a Cell will have a method called render which returns a String representation of the Cell for when we need to print the board. A cell can potentially be rendered as:
-
-# ”.” if the cell has not been fired upon.
-# “M” if the cell has been fired upon and it does not contain a ship (the shot was a miss).
-# “H” if the cell has been fired upon and it contains a ship (the shot was a hit).
-# “X” if the cell has been fired upon and its ship has been sunk.
-# Additionally, we will include an optional boolean argument to indicate if we want to reveal a ship in the cell even if it has not been fired upon. This should render a cell that has not been fired upon and contains a ship as an “S”. This will be useful for showing the user where they placed their ships and for debugging.
-
-# cell_1 = Cell.new("B4")
-# # => #<Cell:0x00007f84f11df920...>
-
-# cell_1.render
-# # => "."
-
-# cell_1.fire_upon
-
-# cell_1.render
-# # => "M"
-
-# cell_2 = Cell.new("C3")
-# # => #<Cell:0x00007f84f0b29d10...>
-
-# cruiser = Ship.new("Cruiser", 3)
-# # => #<Ship:0x00007f84f0ad4fb8...>
-
-# cell_2.place_ship(cruiser)
-
-# cell_2.render
-# # => "."
-
-# # Indicate that we want to show a ship with the optional argument
-# cell_2.render(true)
-# # => "S"
-
-# cell_2.fire_upon
-
-# cell_2.render
-# # => "H"
-
-# cruiser.sunk?
-# # => false
-
-# cruiser.hit
-
-# cruiser.hit
-
-# cruiser.sunk?
-# # => true
-
-# cell_2.render
-# # => "X"
