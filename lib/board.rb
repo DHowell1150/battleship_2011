92d58019cea16ecd5cf26b119f6b782dce22ea69
Dana Howell <howelld115@gmail.com>
2024-01-04 15:42:50 -0700

    I2 Begin framework for Board code.

---
+++ lib/board.rb
@@ -0,0 +1,40 @@
+class Board
+
+  def initialize(data)
+    # @cell_1 = data[:@cell_1]
+    # @cell_2 = data[:@cell_2]
+    # @cell_3 = data[:@cell_3]
+    # @cell_4 = data[:@cell_4]
+    # @cell_5 = data[:@cell_5]
+    # @cell_6 = data[:@cell_6]
+    # @cell_7 = data[:@cell_7]
+    # @cell_8 = data[:@cell_8]
+    # @cell_9 = data[:@cell_9]
+    # @cell_10 = data[:@cell_10]
+    # @cell_11 = data[:@cell_11]
+    # @cell_12 = data[:@cell_12]
+    # @cell_13 = data[:@cell_13]
+    # @cell_14 = data[:@cell_14]
+    # @cell_15 = data[:@cell_15]
+    # @cell_16 = data[:@cell_16]
+
+    
+      # "A1" => @cell_1
+      # "A2" => @cell_2
+      # "A3" => @cell_3
+      # "A4" => @cell_4
+      # "B1" => @cell_5
+      # "B2" => @cell_6
+      # "B3" => #<Cell:0x00007ff0728a3c88...>,
+      # "B4" => #<Cell:0x00007ff0728a3c10...>,
+      # "C1" => #<Cell:0x00007ff0728a3b98...>,
+      # "C2" => #<Cell:0x00007ff0728a3b20...>,
+      # "C3" => #<Cell:0x00007ff0728a3aa8...>,
+      # "C4" => #<Cell:0x00007ff0728a3a30...>,
+      # "D1" => #<Cell:0x00007ff0728a39b8...>,
+      # "D2" => #<Cell:0x00007ff0728a3940...>,
+      # "D3" => #<Cell:0x00007ff0728a38c8...>,
+      # "D4" => #<Cell:0x00007ff0728a3850...>
+      # })
+  end
+end
