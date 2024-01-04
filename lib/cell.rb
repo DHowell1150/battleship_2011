71a82c49b988c07c4ab1e576f19f5ecac4f61da0
Aurora Sanderson <AuroraTuring@gmail.com>
2024-01-03 19:53:23 -0700

    Add #empty, #fired_upon?, and #fire_upon.

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,22 +1,33 @@
 class Cell
-  attr_reader :coordinate, :ship 
+  attr_reader :coordinate, 
+              :ship, 
+              :empty, 
+              :fired_upon
 
   def initialize(coordinate)
     @coordinate = coordinate
-    @ship = ship 
+    @ship = nil 
+    @empty = true 
+    @fired_upon = false 
   end
 
-  # def place_ship()
+  def place_ship(vessel)
+    @ship = vessel 
+    @empty = false 
+  end
 
-  # end
+  def empty?
+    @empty 
+  end
 
-  # def fire_upon
+  def fired_upon?
+    @fired_upon 
+  end
 
-  # end
-
-  # def fired_upon?
-
-  # end
+  def fire_upon
+    @fired_upon = true 
+    @ship.hit 
+  end
 
   # def render
     
