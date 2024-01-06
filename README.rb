898fb11c978670677e98a91b13c15fc06e0555f9
Dana Howell <howelld115@gmail.com>
2024-01-05 17:55:06 -0700

    Add link for helpful tips on rendering S.

--- README.rb
+++ README.rb
@@ -3,3 +3,33 @@
 
 # Aurora's DTR: 
 # https://docs.google.com/document/d/1VfDXQ8ECUcDevZt-RWIg6agt1maEFy6yYZ1yzfuMDD4/edit?usp=sharing
+
+
+#link for how to render "S"
+  https://ideone.com/cM8g57
+
+  #  board = {
+  #   a: [true, true, true, true, false, false, false, false, false, false],
+  #   b: [false, false, false, false, false, false, true, false, false, false],
+  #   c: [true, true, false, false, false, false, false, false, false, false],
+  #   d: [false, false, false, false, true, false, false, false, false, false],
+  #   e: [true, false, false, false, false, false, true, false, false, true],
+  #   f: [true, false, true, false, false, false, true, false, false, true],
+  #   g: [true, false, false, false, false, false, false, false, false, true],
+  #   h: [false, false, false, false, false, false, false, false, false, false],
+  #   i: [false, false, false, false, true, false, true, false, false, false],
+  #   j: [true, true, false, false, true, false, false, false, false, false]
+  # }
+
+# board.each do |key, row|
+# 	string = ''
+# 	row.each do |v|
+# 		if v
+# 			string += ' S'
+# 		else
+# 			string += ' .'
+# 		end
+# 	end
+
+# 	puts key.to_s + ' ' + string
+# end
