e5a24694cf33834315a2f6f8a9af52735d7fd857
Aurora Sanderson <AuroraTuring@gmail.com>
2024-01-05 18:37:27 -0700

    I2 Add it blocks and fixed more @syntax errors.

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -29,19 +29,25 @@ RSpec.describe Board do
 
   describe '#valid_placement' do 
     it 'number of coordinates match ship.length' do
-      expect(board.valid_placement?(cruiser, ["A1", "A2"])).to be false
-      expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to be false
+      expect(@board.valid_placement?(@cruiser, ["A1", "A2"])).to be false
+      expect(@board.valid_placement?(@submarine, ["A2", "A3", "A4"])).to be false
     end
 
     it 'insures the coordinates are consecutive' do
-      expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to be false
-      expect(board.valid_placement?(submarine, ["A1", "C1"])).to be false
-      expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to be false
-      expect(board.valid_placement?(submarine, ["C1", "B1"])).to be false
+      expect(@board.valid_placement?(@cruiser, ["A1", "A2", "A4"])).to be false
+      expect(@board.valid_placement?(@submarine, ["A1", "C1"])).to be false
+      expect(@board.valid_placement?(@cruiser, ["A3", "A2", "A1"])).to be false
+      expect(@board.valid_placement?(@submarine, ["C1", "B1"])).to be false
     end
 
     it 'insures the coordinates are not diagonal' do 
+      expect(@board.valid_placement?(@cruiser, ["A1", "B2", "C3"])).to be false
+      expect(@board.valid_placement?(@submarine, ["C2", "D3"])).to be false
+    end
 
+    it 'the placement should be valid' do 
+      expect(@board.valid_placement?(@submarine, ["A1", "A2"])).to be true
+      expect(@board.valid_placement?(@cruiser, ["B1", "C1", "D1"])).to be true
     end
 
   end
