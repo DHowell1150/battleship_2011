49dc339b8a4bf03b317413cf3d04db0f72b128df
Dana Howell <howelld115@gmail.com>
2024-01-07 13:54:56 -0700

    I2 Add tests for #place_ship.

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -40,18 +40,36 @@ RSpec.describe Board do
       expect(@board.valid_placement?(@cruiser, ["A1", "A2", "A3"])).to be true
       expect(@board.valid_placement?(@submarine, ["A2", "A3", "A4"])).to be false
       expect(@board.valid_placement?(@submarine, ["A2", "A3"])).to be true
-
     end
 
     it 'consecutive_placement' do
-      # consecutive
-      # horizontal
-      # vertical
       expect(@board.valid_placement?(@cruiser, ["B1", "C1", "D1"])).to be true #vertical
       expect(@board.valid_placement?(@cruiser, ["B1", "B2", "B3"])).to be true #horizontal
       expect(@board.valid_placement?(@cruiser, ["A1", "B2", "C3"])).to be false #diagonal
       expect(@board.valid_placement?(@cruiser, ["A3", "A2", "A1"])).to be false #descending
       expect(@board.valid_placement?(@cruiser, ["A1", "A2", "A4"])).to be false #not consecutive
+    end
+  end
+    
+  describe '#place_ship' do
+  #I have access to #place_ship in Cell
+    it 'can place_ship' do
+      @board.place(@cruiser, ["A1", "A2", "A3"])   
+      expect(@cell_1 = board.cells["A1"]).to eq("A1") #??   
+      #<Cell:0x00007fcb0e1f66a8...>
+      expect(@cell_2 = board.cells["A2"]).to eq()
+      #<Cell:0x00007fcb0e1f6630...>
+      expect(@cell_3 = board.cells["A3"]).to eq()
+      #<Cell:0x00007fcb0e1f65b8...>
+      expect(@cell_1.ship).to eq(@cruiser)
+      expect(@cell_2.ship).to eq(@cruiser)
+      expect(@cell_3.ship).to eq(@cruiser)
+      expect(@cell_3.ship == @cell_2.ship).to eq(true)
+    end
+
+    it 'knows if a ship is #overlapping' do
+      @board.place(@cruiser, ["A1", "A2", "A3"])
+      expect(@board.valid_placement?(@submarine, ["A1", "B1"])).to be false
     end
   end
 end
